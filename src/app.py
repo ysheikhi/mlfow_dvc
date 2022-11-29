4b310d058b71d39d9bd818e01455eaae08b78a80
Yousef <joseph.sheikhi@revsolz.com>
2022-11-29 16:15:01 -0700

    connected to gdrive-1

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 import dvc.api
-repo=
-path=
-version = "v3"
+repo="/Users/yousef-mac/Documents/DS_PETPROJECTS/mlflow_demo"
+path="../data/winequality-red.csv"
+version = "v4"
 
 
 if __name__ == "__main__":
-
+    data_url = dvc.api.get_url(path =path, repo=repo, rev=version)
+    print(data_url)
